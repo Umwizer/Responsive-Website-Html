01921f4cb3daf682bba93c6f63c0a917a22601db
Umwizer <ruthumwizerwa@gmail.com>
2025-02-13 08:45:12 +0200

    responsiveness

---
+++ Responsive/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        playfair:"'playfair Display',serif",
+        lato:"'Lato',sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
+
